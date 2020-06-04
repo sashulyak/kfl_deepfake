e8b929acdfb12f8fc979f217660dee0764926063
Alexander <sashulyak@users.noreply.github.com>
2020-06-04 23:37:57 +0300

    Unwrap frames Sequence into flat dataset (#4)
    * Trying to unwrap frame sequence.
    
    * Unwrap frames sequence to flat Dataset.

--- src/dali_dataset.py
+++ src/dali_dataset.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import nvidia.dali.plugin.tf as dali_tf
 import tensorflow as tf
@@ -7,6 +7,7 @@ from nvidia.dali.pipeline import Pipeline
 
 import config
 
+FRAMES_PER_VIDEO = 8
 
 class VideoPipeline(Pipeline):
     """ Nvidia DALI video reader pipeline for using with Tensorflow Dataset."""
@@ -43,7 +44,7 @@ class VideoPipeline(Pipeline):
 
         self.extract = ops.ElementExtract(
             device="gpu",
-            element_map=0
+            element_map=list(range(0, FRAMES_PER_VIDEO))
         )
 
         self.resize = ops.Resize(
@@ -54,10 +55,32 @@ class VideoPipeline(Pipeline):
 
     def define_graph(self):
         frames = self.input(name="Reader")
-        extracted = self.extract(frames)
-        resized = self.resize(extracted)
+        extracted_frames = self.extract(frames)
+        resized_frames = []
+        for extracted_frame in extracted_frames:
+            resized_frames.append(self.resize(extracted_frame))
 
-        return resized
+        return tuple([frame for frame in resized_frames])
+
+
+def unwrap_frames_sequence(*frames) -> tf.data.Dataset:
+    """
+    Unwrap sequence into separate frames with labels.
+
+    :param frames: frames of one video file
+    :return: frames of one video file in Dataset representation
+    """
+    return tf.data.Dataset.from_tensor_slices([frame for frame in frames])
+
+
+def repeat_labels(label) -> tf.data.Dataset:
+    """
+    Repeat each label FRAMES_PER_VIDEO times and flatten
+
+    :param label: label of corresponding video
+    :return: repeated labels
+    """
+    return tf.data.Dataset.from_tensor_slices([label for i in range(FRAMES_PER_VIDEO)])
 
 
 def get_dali_dataset(video_file_paths: List[str], labels: List[int]) -> tf.data.Dataset:
@@ -72,7 +95,7 @@ def get_dali_dataset(video_file_paths: List[str], labels: List[int]) -> tf.data.
         batch_size=1,
         num_threads=2,
         device_id=0,
-        sequence_length=1,
+        sequence_length=FRAMES_PER_VIDEO,
         initial_prefetch_size=16,
         data=video_file_paths,
         shuffle=True
@@ -81,12 +104,16 @@ def get_dali_dataset(video_file_paths: List[str], labels: List[int]) -> tf.data.
     features_dataset = dali_tf.DALIDataset(
         pipeline=video_pipeline,
         batch_size=1,
-        output_shapes=(config.IMG_SIZE, config.IMG_SIZE, 3),
-        output_dtypes=tf.uint8,
+        shapes=[(config.IMG_SIZE, config.IMG_SIZE, 3) for i in range(FRAMES_PER_VIDEO)],
+        dtypes=[tf.uint8 for i in range(FRAMES_PER_VIDEO)],
         device_id=0
     )
 
-    features_dataset_unbatched = features_dataset
-    labels_dataset = tf.data.Dataset.from_tensor_slices(tf.constant(labels))
+    features_dataset = features_dataset.flat_map(unwrap_frames_sequence)
 
-    return tf.data.Dataset.zip((features_dataset_unbatched, labels_dataset))
+    labels_dataset = tf.data.Dataset.from_tensor_slices(labels)
+    labels_dataset = labels_dataset.flat_map(repeat_labels)
+
+    dataset = tf.data.Dataset.zip((features_dataset, labels_dataset))
+
+    return dataset
